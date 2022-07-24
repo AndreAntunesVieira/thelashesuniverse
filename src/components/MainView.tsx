b59a60d3349d0a463d4de05c9d9d86f829bc8001
Andre Antunes Vieira <andreantunesv@gmail.com>
2022-07-24 05:27:48 -0300

    fix pixel

--- src/components/MainView.tsx
+++ src/components/MainView.tsx
@@ -69,9 +69,18 @@ const MainView = ({
           s.parentNode.insertBefore(t,s)}(window, document,'script',
         'https://connect.facebook.net/en_US/fbevents.js');
         fbq('init', '3414661575480290');
-        fbq('track', 'PageView');`}}/>
+        fbq('track', 'PageView');`,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: "none" }}
+          src="https://www.facebook.com/tr?id=3414661575480290&ev=PageView&noscript=1"
+        />
+      </noscript>
     </Head>
-    <noscript><img height="1" width="1" style={{display:'none'}} src="https://www.facebook.com/tr?id=3414661575480290&ev=PageView&noscript=1"/></noscript>
     {children}
   </>
 );
